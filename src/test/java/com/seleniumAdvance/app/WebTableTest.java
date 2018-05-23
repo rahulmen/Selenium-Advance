3757b3b16ea23a6fc7cb0a38fefa250e85e04998
Rahul Mendiratta <rahulmendiratta13@gmail.com>
2018-05-24 00:40:52 +0530

    Added2ndMethodForWebTable

--- src/test/java/com/seleniumAdvance/app/WebTableTest.java
+++ src/test/java/com/seleniumAdvance/app/WebTableTest.java
@@ -4,17 +4,17 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-
-
 
 public class WebTableTest {
 	
 	WebDriver odriver;
 	
-	@BeforeTest
+	@BeforeClass
 	public void setup() {
 		System.setProperty("webdriver.chrome.driver", "D:\\Testing Tools and Jars\\Work\\chromedriver.exe");
 		odriver= new ChromeDriver();
@@ -33,9 +33,21 @@ public class WebTableTest {
 		//SetParticular editor value
 		obj.GetCellEditor(2, 3, 0).sendKeys("10");
 	}
+	
+	@Test
+	public void WebTableTest2() {
+		WebTable obj = new WebTable(odriver.findElement(By.cssSelector("div.cart-info table")));
+		
+		for(int row =1;row<=obj.getRowCount();row++) {
+			for(int cell=1;cell<=obj.getCellCount();cell++) {
+				System.out.print(obj.getCellValueMethod(row, cell)+ " ");
+			}
+			System.out.print("\n");
+		}
+	}
 
 	
-	@AfterTest
+	@AfterClass
 	public void tearDown() {
 		odriver.close();
 	}
